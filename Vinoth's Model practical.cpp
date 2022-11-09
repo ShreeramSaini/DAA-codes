f3c74c0011281fe70a0e48b080fcb8f3ab58d137
SHREERAM SAINI <92785217+ShreeramSaini@users.noreply.github.com>
2022-11-10 02:13:48 +0530

    Add files via upload

---
+++ Vinoth's Model practical.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){ 
+ int n,t,I,count=0; 
+ cin>>n>>t; 
+ int arr[n]; 
+ for(i=0;i<n;i++){ 
+ cin>>arr[i]; 
+ } 
+ sort(arr,arr+n); 
+ for(i=0;i<n;i++){ 
+ t-=arr[i]; 
+ if(t<0){ 
+ break; 
+ } 
+ count++; 
+ } 
+ cout<<count; 
+ return 0;
+} 
